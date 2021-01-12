91be6113e7744ea0fce86451e94cad2f46a9a296
Will <will.lucen4@gmail.com>
2021-01-12 14:54:19 -0300

    layout changes

--- src/components/VStep.vue
+++ src/components/VStep.vue
@@ -13,15 +13,18 @@
       </div>
     </slot>
 
+    <hr />
+
     <slot name="actions">
-      <div class="v-step__buttons">
-        <button @click.prevent="skip" v-if="!isLast && isButtonEnabled('buttonSkip')" class="v-step__button v-step__button-skip">{{ labels.buttonSkip }}</button>
-        <button @click.prevent="previousStep" v-if="!isFirst && isButtonEnabled('buttonPrevious')" class="v-step__button v-step__button-previous">{{ labels.buttonPrevious }}</button>
-        <button @click.prevent="nextStep" v-if="!isLast && isButtonEnabled('buttonNext')" class="v-step__button v-step__button-next">{{ labels.buttonNext }}</button>
-        <button @click.prevent="finish" v-if="isLast && isButtonEnabled('buttonStop')" class="v-step__button v-step__button-stop">{{ labels.buttonStop }}</button>
+      <div>
+        <button @click.prevent="skip" v-if="!isLast && isButtonEnabled('buttonSkip')" class="v-step__button v-step__button-skip">x</button>
+        <div class="v-step__buttons">
+          <button @click.prevent="previousStep" v-if="!isFirst && isButtonEnabled('buttonPrevious')" class="v-step__button v-step__button-previous">{{ labels.buttonPrevious }}</button>
+          <button @click.prevent="nextStep" v-if="!isLast && isButtonEnabled('buttonNext')" class="v-step__button v-step__button-next">{{ labels.buttonNext }}</button>
+          <button @click.prevent="finish" v-if="isLast && isButtonEnabled('buttonStop')" class="v-step__button v-step__button-stop">{{ labels.buttonStop }}</button>
+        </div>
       </div>
     </slot>
-
     <div class="v-step__arrow" :class="{ 'v-step__arrow--dark': step.header && step.header.title }"></div>
   </div>
 </template>
@@ -193,129 +196,162 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .v-step {
-    background: #50596c; /* #ffc107, #35495e */
-    color: white;
-    max-width: 320px;
-    border-radius: 3px;
-    filter: drop-shadow(0 0 2px rgba(0, 0, 0, 0.5));
-    padding: 1rem;
-    text-align: center;
-    z-index: 10000;
+.v-step {
+  background: white; /* #ffc107, #35495e */
+  color: white;
+  max-width: 320px;
+  border-radius: 3px;
+  filter: drop-shadow(0 0 2px rgba(0, 0, 0, 0.5));
+  padding: 1rem;
+  text-align: center;
+  z-index: 10000;
+
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: normal;
+  font-size: 12px;
+  color: #6B6D6D;
+}
+
+.v-step .v-step__arrow {
+  width: 0;
+  height: 0;
+  border-style: solid;
+  position: absolute;
+  margin: 0.5rem;
+}
+
+.v-step .v-step__arrow {
+  border-color: white; /* #ffc107, #35495e */
+
+  &--dark {
+    border-color: #454d5d;
   }
+}
 
-  .v-step .v-step__arrow {
-    width: 0;
-    height: 0;
-    border-style: solid;
-    position: absolute;
-    margin: 0.5rem;
+.v-step[x-placement^="top"] {
+  margin-bottom: 0.5rem;
+}
+
+.v-step[x-placement^="top"] .v-step__arrow {
+  border-width: 0.5rem 0.5rem 0 0.5rem;
+  border-left-color: transparent;
+  border-right-color: transparent;
+  border-bottom-color: transparent;
+  bottom: -0.5rem;
+  left: calc(50% - 1rem);
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+.v-step[x-placement^="bottom"] {
+  margin-top: 0.5rem;
+}
+
+.v-step[x-placement^="bottom"] .v-step__arrow {
+  border-width: 0 0.5rem 0.5rem 0.5rem;
+  border-left-color: transparent;
+  border-right-color: transparent;
+  border-top-color: transparent;
+  top: -0.5rem;
+  left: calc(50% - 1rem);
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+.v-step[x-placement^="right"] {
+  margin-left: 0.5rem;
+}
+
+.v-step[x-placement^="right"] .v-step__arrow {
+  border-width: 0.5rem 0.5rem 0.5rem 0;
+  border-left-color: transparent;
+  border-top-color: transparent;
+  border-bottom-color: transparent;
+  left: -0.5rem;
+  top: calc(50% - 1rem);
+  margin-left: 0;
+  margin-right: 0;
+}
+
+.v-step[x-placement^="left"] {
+  margin-right: 0.5rem;
+}
+
+.v-step[x-placement^="left"] .v-step__arrow {
+  border-width: 0.5rem 0 0.5rem 0.5rem;
+  border-top-color: transparent;
+  border-right-color: transparent;
+  border-bottom-color: transparent;
+  right: -0.5rem;
+  top: calc(50% - 1rem);
+  margin-left: 0;
+  margin-right: 0;
+}
+
+/* Custom */
+
+.v-step__header {
+  margin: -1rem -1rem 0.5rem;
+  padding: 0.5rem;
+  background-color: #454d5d;
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+}
+
+.v-step__content {
+  margin: 0 0 1rem 0;
+}
+
+.v-step__buttons{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.v-step__button {
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  outline: none;
+  text-align: center;
+  text-decoration: none;
+  transition: all .2s ease;
+  vertical-align: middle;
+  white-space: nowrap;
+
+  font-weight: 500;
+  font-size: 18px;
+  text-align: right;
+  text-decoration: underline;
+
+  color: #E57B39;
+
+  &:hover {
+    background-color: rgba(white, 0.95);
+    color: #50596c;
   }
+}
 
-  .v-step .v-step__arrow {
-    border-color: #50596c; /* #ffc107, #35495e */
+.v-step__button-previous {
+  font-size: 14px;
+  text-decoration: underline;
+  color: #979797;
+}
 
-    &--dark {
-      border-color: #454d5d;
-    }
-  }
+.v-step__button-next:only-child {
+  width: 100%;
+}
 
-  .v-step[x-placement^="top"] {
-    margin-bottom: 0.5rem;
-  }
-
-  .v-step[x-placement^="top"] .v-step__arrow {
-    border-width: 0.5rem 0.5rem 0 0.5rem;
-    border-left-color: transparent;
-    border-right-color: transparent;
-    border-bottom-color: transparent;
-    bottom: -0.5rem;
-    left: calc(50% - 1rem);
-    margin-top: 0;
-    margin-bottom: 0;
-  }
-
-  .v-step[x-placement^="bottom"] {
-    margin-top: 0.5rem;
-  }
-
-  .v-step[x-placement^="bottom"] .v-step__arrow {
-    border-width: 0 0.5rem 0.5rem 0.5rem;
-    border-left-color: transparent;
-    border-right-color: transparent;
-    border-top-color: transparent;
-    top: -0.5rem;
-    left: calc(50% - 1rem);
-    margin-top: 0;
-    margin-bottom: 0;
-  }
-
-  .v-step[x-placement^="right"] {
-    margin-left: 0.5rem;
-  }
-
-  .v-step[x-placement^="right"] .v-step__arrow {
-    border-width: 0.5rem 0.5rem 0.5rem 0;
-    border-left-color: transparent;
-    border-top-color: transparent;
-    border-bottom-color: transparent;
-    left: -0.5rem;
-    top: calc(50% - 1rem);
-    margin-left: 0;
-    margin-right: 0;
-  }
-
-  .v-step[x-placement^="left"] {
-    margin-right: 0.5rem;
-  }
-
-  .v-step[x-placement^="left"] .v-step__arrow {
-    border-width: 0.5rem 0 0.5rem 0.5rem;
-    border-top-color: transparent;
-    border-right-color: transparent;
-    border-bottom-color: transparent;
-    right: -0.5rem;
-    top: calc(50% - 1rem);
-    margin-left: 0;
-    margin-right: 0;
-  }
-
-  /* Custom */
-
-  .v-step__header {
-    margin: -1rem -1rem 0.5rem;
-    padding: 0.5rem;
-    background-color: #454d5d;
-    border-top-left-radius: 3px;
-    border-top-right-radius: 3px;
-  }
-
-  .v-step__content {
-    margin: 0 0 1rem 0;
-  }
-
-  .v-step__button {
-    background: transparent;
-    border: .05rem solid white;
-    border-radius: .1rem;
-    color: white;
-    cursor: pointer;
-    display: inline-block;
-    font-size: .8rem;
-    height: 1.8rem;
-    line-height: 1rem;
-    outline: none;
-    margin: 0 0.2rem;
-    padding: .35rem .4rem;
-    text-align: center;
-    text-decoration: none;
-    transition: all .2s ease;
-    vertical-align: middle;
-    white-space: nowrap;
-
-    &:hover {
-      background-color: rgba(white, 0.95);
-      color: #50596c;
-    }
-  }
+.v-step__button-skip{
+  position: absolute;
+  top: -.3rem;
+  right: -.3rem;
+  border-radius: 50%;
+  background-color: white;
+  padding: 0 8px;
+  z-index: 9999;
+  color: #003F7A;
+  text-decoration: none;
+}
 </style>
